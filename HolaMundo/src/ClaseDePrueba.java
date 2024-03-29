4f8b7d6842533184d6c768a376e0850e2eb0998b
Erick-byte4389 <erick_1322134389@uptecamac.edu.mx>
2024-03-29 16:27:57 -0600

    imprimiendo un mensaje
    Probando un mensaje de prueba desde mi pc

--- HolaMundo/src/ClaseDePrueba.java
+++ HolaMundo/src/ClaseDePrueba.java
@@ -1,5 +1,7 @@
 public class ClaseDePrueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde la clase de prueba");
+
+        System.out.println("Hola desde mi pc");
     }
 }
