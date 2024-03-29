9cba1e237d22ca718404021200cd30da9b7db663
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-03-29 16:12:29 -0600

    (feat) Creando nueva clase
    Se creó la nueva clase "Caracteres"

---
+++ HolaMundo/src/Caracteres.java
@@ -0,0 +1,7 @@
+public class Caracteres {
+    public static void main(String[] args) {
+
+        char caracteres = 'E'; // Basado en estándar unicode
+        System.out.println("caracteres = " + caracteres);
+    }
+}
